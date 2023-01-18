18f7b5c03e803ef8a834f6bb400ae50e5bf06b63
Misha <vornov-misha@yandex.ru>
2023-01-18 21:46:25 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout << "Hello World my programsssss" << endl;
+    cin >> a;
+    cout << "a=\t" << a << endl;
+    system("pause");
+    return 0;
+}
